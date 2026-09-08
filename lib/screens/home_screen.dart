0404fe96ee58c2f9fad203b83024aad42fc5d5ce
hlengiwe1206-dotcom <hlengiwe1206@gmail.com>
2026-09-08 10:56:17 +0200

    Add GitHub Actions workflow to unpack Flutter project

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sisonke'),
+      ),
+      body: const Center(
+        child: Text('Home Screen'),
+      ),
+    );
+  }
+}
